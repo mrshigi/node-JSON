a19e1a22c943215ca40b20d27cda3b156cadc1f6
mrshigi <sraudat@email.sc.edu>
2023-12-08 17:58:46 -0500

    a13

---
+++ public/script.js
@@ -0,0 +1,54 @@
+const showStocks = async () => 
+{
+    const stocksJSON = await getJSON();
+    if(stocksJSON == " ") {
+        return;
+    }
+  
+    
+    console.log("praying this works");
+    let stockDiv = document.getElementById("stock-list");
+
+    stocksJSON.forEach((stocks) => {
+        let section = document.createElement("section");
+        stockDiv.append(section);
+        let title = stocks.name;
+
+        let h3 = document.createElement("h3");
+        section.append(h3);
+        h3.innerHTML = stocks.name;
+        
+        let img = document.createElement("img");
+        section.append(img);
+        img.src="http://localhost:3000/" +stocks.img;
+
+        let p = document.createElement("p");
+        section.append(p);
+        p.innerHTML = stocks.price;
+        let p1 = document.createElement("p");
+        section.append(p1);
+        p1.innerHTML = stocks.fiftytwoweekhigh;
+        let p2 = document.createElement("p");
+        section.append(p2);
+        p2.innerHTML = stocks.fiftytwoweeklow;
+        let p3 = document.createElement("p");
+        section.append(p3);
+        p3.innerHTML = stocks.marketcap;
+    });
+
+
+};
+
+const getJSON = async () => {
+    try
+    {
+        let response = await fetch("http://localhost:3000/api/stocks");
+        return await response.json();
+    }catch(error){
+        console.log("error retrieving json");
+        return " ";
+    }
+}
+window.onload = ()  => {
+    showStocks();
+}
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/stocks", (req, res) => {
+  const stocks = [];
+  stocks[0] = {
+    name: "AMD",
+    price: "Price: 129",
+    fiftytwoweekhigh: "52-Week High: 135",
+    fiftytwoweeklow: "52-Week Low: 82",
+    marketcap: [
+        "Details",
+        "Market Cap: 600B",
+        "Employee Count: 2346", 
+        "Price to Earnings Ratio: 88"
+    ],
+    img: "images/AMD.png",
+  };
+  stocks[1] = {
+    name: "NVDA",
+    price: "Price: 475",
+    fiftytwoweekhigh: "52-Week High: 540",
+    fiftytwoweeklow: "52-Week Low: 420",
+    marketcap: [
+        "Details",
+        "Market Cap: 600B",
+        "Employee Count: 2346", 
+        "Price to Earnings Ratio: 88"
+    ],
+    img: "images/NVDA.png",
+  };
+  stocks[2] = {
+    name: "BAC",
+    price: "Price: 31",
+    fiftytwoweekhigh: "52-Week High: 34",
+    fiftytwoweeklow: "52-Week Low: 25",
+    marketcap: [
+        "Details",
+        "Market Cap: 600B",
+        "Employee Count: 2346", 
+        "Price to Earnings Ratio: 88"
+    ],
+    img: "images/BAC.png",
+  };
+  stocks[3] = {
+    name: "XOM",
+    price: "Price: 79",
+    fiftytwoweekhigh: "52-Week High: 135",
+    fiftytwoweeklow: "52-Week Low: 82",
+    marketcap: [
+        "Details",
+        "Market Cap: 600B",
+        "Employee Count: 2346", 
+        "Price to Earnings Ratio: 88"
+    ],
+    img: "images/XOM.png",
+  };
+  stocks[4] = {
+    name: "SPY",
+    price: "Price: 425",
+    fiftytwoweekhigh: "52-Week High: 460",
+    fiftytwoweeklow: "52-Week Low: 410",
+    marketcap: [
+        "Details",
+        "Market Cap: 600B",
+        "Employee Count: 2346", 
+        "Price to Earnings Ratio: 88"
+    ],
+    img: "images/SPY.png",
+  };
+  res.json(stocks);
+});
+
+app.listen(3000, () => {
+  console.log("I listen");
+});
