0041d8a2ae1894ba70399ec708eedd4a0972a1b2
mrshigi <sraudat@email.sc.edu>
2023-12-12 21:55:29 -0500

    formatting

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
+const multer = require("multer");
 app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+const upload = multer({ dest: __dirname + "/public/images" });
 
 app.get("/api/stocks", (req, res) => {
   const stocks = [];
